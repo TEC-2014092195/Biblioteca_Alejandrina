4bb332bbdf6e560b18ca2483a941c38b42825db3
Jasson Moya <Jasson@172.26.103.81>
2014-09-25 01:18:12 -0600

    Para cargar imágenes

---
+++ Biblioteca_Alejandrina/src/para_Guardar/Guardar_Imagen.java
@@ -0,0 +1,35 @@
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+/**====================================================================================
+ * Archivo      : Guardar_Imagen.java » Paquete: para_Guardar » Proyecto: Biblioteca_Alejandrina
+ * Autores      : Kevin Hernández Rostrán, Jasson Moya Álvarez, 
+ *				  Julián Méndez Oconitrillo, José Aguilar Quesada.
+ * Curso        : Programación Orientada a Objetos - Instituto Tecnológico de Costa Rica
+ * Descripcion  : Control de préstamo de artículos para una Biblioteca
+ **==================================================================================== 
+ */
+
+	
+
+	//Kevin, este es el algoritmo, tiene que importar lo de bufferedImage, y lo IOnoséqué... Hay que buscar una manera de
+	//automatizar los nombre de las imágenes. Lo que yo tenía en mente era usar números para guardar la vara, por ejemplo,
+	//la primera imagen se llamaría book1, la segunda book2, la tercera book3. Esto se haría para poder manejar las imágenes
+	//más fácilmente.
+
+/*	}else if(e.getSource()==btnGuardar){
+			System.out.println("Boton guardar");
+			guardarImagen(selectedFile);
+		}
+		
+	}
+	
+	public void guardarImagen(File archivo){
+		try{
+			nueva_imagen = ImageIO.read(archivo);
+			System.out.println("Se leyó la imagen");
+			ImageIO.write(nueva_imagen, ex, new File("C://Users/Jasson/Desktop/Recurso/" + "book1" + "." +ex));
+			System.out.println("Se escribió correctamente");
+		}catch(Exception e){e.printStackTrace();}
+	}*/
